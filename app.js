402d14b7c864260c6201c5b2d70821a4b2a6db8c
parkpoomd <parkpoom.d@gmail.com>
2019-10-19 15:43:25 +0700

    6. Working with Dynamic Content & Adding Templating Engines

--- app.js
+++ app.js
@@ -2,20 +2,29 @@ const path = require('path')
 
 const express = require('express')
 const bodyParser = require('body-parser')
+// const expressHbs = require('express-handlebars')
 
 const app = express()
 
-const adminRoutes = require('./routes/admin')
+// app.engine(
+//   'handlebars',
+//   expressHbs({ layoutsDir: 'views/layouts/', defaultLayout: 'main' })
+// )
+// app.set('view engine', 'handlebars')
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminRoutes)
+app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+  res.status(404).render('404', { pageTitle: 'Page Not Found!', path: null })
 })
 
 app.listen(3000)
--- routes/shop.js
+++ routes/shop.js
@@ -6,8 +6,18 @@ const rootDir = require('../util/path')
 
 const router = express.Router()
 
+const adminData = require('./admin')
+
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+  const products = adminData.products
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  })
 })
 
 module.exports = router
