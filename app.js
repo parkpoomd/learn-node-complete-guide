4826c6acfb8def5afb00f667b98d8e6412020802
parkpoomd <parkpoom.d@gmail.com>
2019-10-19 21:01:46 +0700

    finish code Optional Enhancing the App

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 // const expressHbs = require('express-handlebars')
 
+const errorController = require('./controllers/error')
+
 const app = express()
 
 // app.engine(
@@ -23,8 +25,6 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found!', path: null })
-})
+app.use(errorController.get404)
 
 app.listen(3000)
