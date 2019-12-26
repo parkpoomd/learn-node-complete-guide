60955502e47748290888bb2ffaf3cee6c0aa4c71
parkpoomd <parkpoom.d@gmail.com>
2019-12-26 17:08:47 +0700

    Linking to the Edit Page

--- controllers/admin.js
+++ controllers/admin.js
@@ -37,9 +37,11 @@ exports.getEditProduct = (req, res, next) => {
   });
 };
 
+exports.postEditProduct = (req, res, next) => {};
+
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
-    res.render("admin/product-list", {
+    res.render("admin/products", {
       prods: products,
       pageTitle: "Admin Products",
       path: "/admin/product-list"
