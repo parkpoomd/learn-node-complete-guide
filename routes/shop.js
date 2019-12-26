980d9b3c1c9e0227272524c442b46a30ebb21c0e
parkpoomd <parkpoom.d@gmail.com>
2019-12-26 14:41:21 +0700

    Extracting Dynamic Params

--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,13 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const shopController = require('../controllers/shop')
+const shopController = require("../controllers/shop");
 
-router.get('/', shopController.getIndex)
-router.get('/products', shopController.getProducts)
-router.get('/cart', shopController.getCart)
-router.get('/orders', shopController.getOrders)
-router.get('checkout', shopController.getCheckout)
+router.get("/", shopController.getIndex);
+router.get("/products", shopController.getProducts);
+router.get("/products/:productId", shopController.getProduct);
+router.get("/cart", shopController.getCart);
+router.get("/orders", shopController.getOrders);
+router.get("checkout", shopController.getCheckout);
 
-module.exports = router
+module.exports = router;
